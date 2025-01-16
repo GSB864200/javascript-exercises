9af3d7911b87a3eaf98d0f938d8559a971e2f288
GSB864200 <gsb864200@gmail.com>
2025-01-16 23:14:14 +0530

    gsb

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sumo = 0;
+  for (let i = 0; i < array.length ; i++){
+    sumo += array[i];
+  }
+  return sumo;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let mut = 1;
+  for (let i = 0 ; i < array.length ; i++){
+    mut *= array[i];
+    
+  }
+  return mut ;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
