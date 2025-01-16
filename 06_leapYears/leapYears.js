a944662eeae8b73527c3d2b0e22fbbfc771d2eb4
GSB864200 <gsb864200@gmail.com>
2025-01-16 22:29:30 +0530

    gsb

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    const isYearDivBy4 = year % 4 === 0 ; 
+    const isYearDivBy100 = year % 100 === 0 ; 
+    const isYearDivBy400 = year % 400 === 0
+
+
+
+    if ( isYearDivBy4 && (!isYearDivBy100 || isYearDivBy400)){
+         return true;
+    }else{
+        return false;
+     } 
 };
 
 // Do not edit below this line
