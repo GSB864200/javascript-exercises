47187ba38a4de5f3cc4fe042da1c4a03ef5199b7
GSB864200 <gsb864200@gmail.com>
2025-01-16 17:13:23 +0530

    gsb

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(a, ...args) {
+
+    const newArray = [];
+
+    a.forEach(element => {
+        if (!args.includes(element)){
+            newArray.push(element);
+        }
+    });
+
+    return newArray;
+
 };
 
 // Do not edit below this line
