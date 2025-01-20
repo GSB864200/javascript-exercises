244fae7b992ef95333077beab8540371bb2b3755
GSB864200 <gsb864200@gmail.com>
2025-01-20 13:08:30 +0530

    gsb

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function(birth, death){
+        if(!death){
+            death = new Date().getFullYear()}
+        return death - birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldes, currentPerson) => {
+        const oldestAge = getAge(oldes.yearOfBirth, oldes.yearOfDeath);
+        curentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+     return oldestAge < curentAge ? currentPerson : oldes;
+
+    });
 };
 
 // Do not edit below this line
